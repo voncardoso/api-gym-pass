6d8488b5faebe40f97018146a407cd488c928bb8
Von Harrison Tadaiesky <vonharrison26@gmail.com>
2025-03-30 21:27:46 -0300

    feat: implement check-in use case and repositories

---
+++ src/repositories/prisma/prisma-check-ins-repository.ts
@@ -0,0 +1,10 @@
+import { Prisma, CheckIn } from "@prisma/client";
+import { prisma } from "@/lib/prisma";
+import { CheckInsRepository } from "../check-ins-repository";
+
+export class PrismaCheckInsRepository implements CheckInsRepository {
+  async create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn> {
+    const checkIn = await prisma.checkIn.create({ data });
+    return checkIn;
+  }
+}
